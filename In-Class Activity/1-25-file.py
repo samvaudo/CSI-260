67aa7f4748886b23b85b367cc343819b5f47b448
samvaudo <112639285+samvaudo@users.noreply.github.com>
2024-01-25 15:46:12 -0500

    end of class

---
+++ In-Class Activity/1-25-file.py
@@ -0,0 +1,15 @@
+"""
+Author: Samuel Vaudo
+Class: CSI-260-01
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given fully documented
+references to the work of others. I understand the definition and consequences of
+plagiarism and acknowledge that the assessor of this assignment may, for the purpose of
+assessing this assignment reproduce this assignment and provide a copy to another member
+of academic staff and / or communicate a copy of this assignment to a plagiarism
+service(which may then retain a copy of this assignment on its database for the purpose
+of future plagiarism checking).
+"""
+
+
+
